716f37704c53afa31402f3455c773f7801d98160
VVik61 <vikivvoron@Gmail.com>
2025-09-16 15:02:18 +0300

    in local - templates&static

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
 
 def index(request):
     # type: (HttpRequest) -> HttpResponse
+
     """Simple view that renders a template."""
     context = {"message": "Привет, Django!"}
     return render(request, "myapp/index.html", context)
